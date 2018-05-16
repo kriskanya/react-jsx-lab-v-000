38d663ae5ba924caba8cd53f0eebd79e251a1f0d
Kristian Kanya <kriskanya@gmail.com>
2018-05-16 15:07:57 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,6 +1,12 @@
 import React from 'react';
+<<<<<<< HEAD
 
 export default class FillerText extends React.Component {
+=======
+import ReactDOM from 'react-dom';
+
+export class FillerText extends React.Component {
+>>>>>>> ead36fe27fdb125111ed9402e39eb28a534f0b77
   render() {
     return (
       <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,12 @@
 import React from 'react';
+<<<<<<< HEAD
 
 export default class RegistrationForm extends React.Component {
+=======
+import ReactDOM from 'react-dom';
+
+export class RegistrationForm extends React.Component {
+>>>>>>> ead36fe27fdb125111ed9402e39eb28a534f0b77
   render() {
     return (
       <form>
